507a10a4e7865382d4eaaabb8c87783e9f3b83ea
ohju96 <ohju96@naver.com>
2023-01-09 23:51:51 +0900

    [문법] : struct 3예제부터 시작

--- section8/sturct3.go
+++ section8/sturct3.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	exe1()
+
+	fmt.Println("Main Routine Start", time.Now())
+	go exe2()
+	go exe3()
+	time.Sleep(4 * time.Second)
+	fmt.Println("Main Routine end", time.Now())
+
+}
+
+func exe1() {
+	fmt.Println("exe1 func start", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("exe1 func end", time.Now())
+}
+
+func exe2() {
+	fmt.Println("exe2 func start", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("exe2 func end", time.Now())
+}
+
+func exe3() {
+	fmt.Println("exe3 func start", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("exe3 func end", time.Now())
 }
