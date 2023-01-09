972f89a441ff1f0d71ec2461e38b770ffba3f35b
ohju96 <ohju96@naver.com>
2023-01-09 14:45:53 +0900

    [문법] : struct v.3

---
+++ section9/struct11.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	name   string
+	salary float64
+	bonus  float64
+}
+
+// 구조체 임베디드 패턴
+type Executives struct { // 구조체
+	Employee     // is a 관계 : 임원도 직원이다.
+	specialBonus float64
+}
+
+func (e Employee) Calculate() float64 {
+	return e.salary + e.bonus
+}
+
+func (e Executives) Calculate() float64 {
+	return e.salary + e.bonus + e.specialBonus
+}
+
+func main() {
+
+	ep1 := Employee{"oh", 100, 10}
+	ep2 := Employee{"kim", 200, 20}
+	ex := Executives{Employee{"lee", 300, 30}, 30}
+
+	fmt.Println(ep1.Calculate())
+	fmt.Println(ep2.Calculate())
+
+	fmt.Println(ex.Calculate())
+	fmt.Println(int(ex.Employee.Calculate() + ex.specialBonus))
+}
